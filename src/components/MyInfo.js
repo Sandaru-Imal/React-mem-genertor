3506b97f694b76f2f92e7ca4e0f094deaaf26300
Sandaru Imal <43520816+Sandaru-Imal@users.noreply.github.com>
2020-07-07 21:36:12 +0530

    initial commit

---
+++ src/components/MyInfo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Hello I am Sandaru and i like to travel</h1>
+      <ul>
+        <li>Sri Lanka</li>
+        <li>Russia</li>
+        <li>Vietnam</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyInfo;
